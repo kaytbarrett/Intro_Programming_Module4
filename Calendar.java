851fc42bb7946695dd438f03eb7488b224d9cb1d
Katie Barrett <kayt.barrett22@gmail.com>
2024-06-20 16:34:37 -0600

    Added comments to calendars

--- Calendar.java
+++ Calendar.java
@@ -34,10 +34,16 @@ public class Calendar {
 
     public static void printMonthCalendar(int m, int y)
     {
+        // Set day always equal to 1
         int d = 1;
+
+        // Call the getStartDay method to get the day of the week that this month/year starts on
         int startDay = getStartDay(m, d, y);
+
+        // Call the getNumDaysInMonth method to get the number of days in that month
         int numDaysInMonth = getNumDaysInMonth(m, y);
 
+        // Print out the Month Header and Body, with some extra spacing with new lines
         System.out.println("\n");
         printMonthHeader(m, y);
         printMonthBody(startDay, numDaysInMonth);
@@ -64,10 +70,12 @@ public class Calendar {
         int dayNum = ( ( h + 5 ) % 7 ) + 1;     
         
         return dayNum;
-  }
+    }
 
+    
     public static void printMonthHeader(int m, int y)
     {
+        // Get the string of the month Name to print in the header with the year
         String monthName = getMonthName(m);
         System.out.println("\t" + monthName + " " + y);
         System.out.println("-----------------------------");
@@ -75,41 +83,49 @@ public class Calendar {
 
     public static void printMonthBody(int startDay, int numDaysInMonth)
     {
+        // Print out the days of the week
         System.out.print(" Sun Mon Tue Wed Thu Fri Sat");
         System.out.println();
-        int lines = 0;
 
+        // Declare and initialize a variable called spacing to keep track of spacing for a new line
+        int spacing = 0;
+
+        // Print blank spaces for week days that come before the start day
         if (startDay == 7 )
         {
-            // Do nothing
+            // Do nothing, this is Sun so that is really day 1 of our calendar
         } else 
         {
+            // Create blank spaces with no number
             for (int j = 0; j < startDay; j++)
             {
                 System.out.print("    ");
-                lines++;
+                spacing++;
             }
         }
         
-
+        // Print out each day of the month under the correct weekday
         for (int i = 1; i <= numDaysInMonth; i++)
         {
-            if (lines % 7 == 0 && lines != 0) {
+            // Create a new line if we get to the end of the week
+            if (spacing % 7 == 0 && spacing != 0) {
                 System.out.println();
             }
 
+            // Print out the correct day with the correct spacing
             if (i < 10) {
                 System.out.print("   " + i );
             } else {
                 System.out.print("  " + i );
             }
     
-            lines++;
+            spacing++;
         }
     }
 
     public static String getMonthName(int m)
     {
+        // Get the month name using a switch statement for months 1-12
         String monthName = "None";
         switch (m) {
             case 1: monthName = "January";
@@ -143,9 +159,12 @@ public class Calendar {
 
     public static int getNumDaysInMonth( int m, int y)
     {
+        // Declare and initialize the number of days in a month
         int numDaysInMonth = 0;
+        // Call the isLeapYear method to determine if the input year is leap or not
         boolean isLeapYear = isLeapYear(y);
 
+        // Determine the number of days in the month based on month and whether or not it's a leap year
         if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
         {
             numDaysInMonth = 31;
@@ -165,9 +184,10 @@ public class Calendar {
 
     public static boolean isLeapYear( int y )
     {
-        if ( y % 4 == 0 )
+        // Determine if a year is a leap year, return true if it is and false if it is not
+        if ( y % 4 == 0 )           // Leap year must be divisible by 4
         {
-            if ( y % 100 == 0)
+            if ( y % 100 == 0)      // Leap year must not be divisible by 100 unless it's also divisible by 400
             {
                 if ( y % 400 == 0)
                 {
--- YearCalendar.java
+++ YearCalendar.java
@@ -36,10 +36,16 @@ public class YearCalendar {
 
     public static void printMonthCalendar(int m, int y)
     {
+        // Set day always equal to 1
         int d = 1;
+
+        // Call the getStartDay method to get the day of the week that this month/year starts on
         int startDay = getStartDay(m, d, y);
+
+        // Call the getNumDaysInMonth method to get the number of days in that month
         int numDaysInMonth = getNumDaysInMonth(m, y);
 
+        // Print out the Month Header and Body, with some extra spacing with new lines
         System.out.println();
         printMonthHeader(m, y);
         printMonthBody(startDay, numDaysInMonth);
@@ -66,10 +72,12 @@ public class YearCalendar {
         int dayNum = ( ( h + 5 ) % 7 ) + 1;     
         
         return dayNum;
-  }
+    }
 
+    
     public static void printMonthHeader(int m, int y)
     {
+        // Get the string of the month Name to print in the header with the year
         String monthName = getMonthName(m);
         System.out.println("\t" + monthName + " " + y);
         System.out.println("-----------------------------");
@@ -77,41 +85,49 @@ public class YearCalendar {
 
     public static void printMonthBody(int startDay, int numDaysInMonth)
     {
+        // Print out the days of the week
         System.out.print(" Sun Mon Tue Wed Thu Fri Sat");
         System.out.println();
-        int lines = 0;
 
+        // Declare and initialize a variable called spacing to keep track of spacing for a new line
+        int spacing = 0;
+
+        // Print blank spaces for week days that come before the start day
         if (startDay == 7 )
         {
-            // Do nothing
+            // Do nothing, this is Sun so that is really day 1 of our calendar
         } else 
         {
+            // Create blank spaces with no number
             for (int j = 0; j < startDay; j++)
             {
                 System.out.print("    ");
-                lines++;
+                spacing++;
             }
         }
         
-
+        // Print out each day of the month under the correct weekday
         for (int i = 1; i <= numDaysInMonth; i++)
         {
-            if (lines % 7 == 0 && lines != 0) {
+            // Create a new line if we get to the end of the week
+            if (spacing % 7 == 0 && spacing != 0) {
                 System.out.println();
             }
 
+            // Print out the correct day with the correct spacing
             if (i < 10) {
                 System.out.print("   " + i );
             } else {
                 System.out.print("  " + i );
             }
     
-            lines++;
+            spacing++;
         }
     }
 
     public static String getMonthName(int m)
     {
+        // Get the month name using a switch statement for months 1-12
         String monthName = "None";
         switch (m) {
             case 1: monthName = "January";
@@ -145,9 +161,12 @@ public class YearCalendar {
 
     public static int getNumDaysInMonth( int m, int y)
     {
+        // Declare and initialize the number of days in a month
         int numDaysInMonth = 0;
+        // Call the isLeapYear method to determine if the input year is leap or not
         boolean isLeapYear = isLeapYear(y);
 
+        // Determine the number of days in the month based on month and whether or not it's a leap year
         if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
         {
             numDaysInMonth = 31;
@@ -167,9 +186,10 @@ public class YearCalendar {
 
     public static boolean isLeapYear( int y )
     {
-        if ( y % 4 == 0 )
+        // Determine if a year is a leap year, return true if it is and false if it is not
+        if ( y % 4 == 0 )           // Leap year must be divisible by 4
         {
-            if ( y % 100 == 0)
+            if ( y % 100 == 0)      // Leap year must not be divisible by 100 unless it's also divisible by 400
             {
                 if ( y % 400 == 0)
                 {
